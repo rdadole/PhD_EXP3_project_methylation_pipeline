b64ea48f95174ec9e0a4333af17c6da22f20be29
rdadole <ronandadole@gmail.com>
2025-10-09 10:37:53 +0200

    Added reference genome indexing

---
+++ preparations/genome_indexing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p compute
+#SBATCH --job-name=Indexing
+#SBATCH --mem=16G
+#SBATCH --ntasks=1
+#SBATCH -t 1-0:00
+#SBATCH --cpus-per-task=8
+#SBATCH --output=indexing.out
+#SBATCH --error=indexing.err
+
+
+module load all gencore/3
+module load samtools/1.21
+
+#Path to the reference genome
+reference="/scratch/rd3725/nanopore/ref/GDDH13_1-1_formatted.fasta"
+
+samtools faidx -@ 8 $reference
