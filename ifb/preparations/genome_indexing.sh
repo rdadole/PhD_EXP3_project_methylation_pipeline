001ceebec6c9ead7c3199e8e78380d9be570cd52
rdadole <ronandadole@gmail.com>
2026-04-24 11:32:03 +0200

    preparation for ifb

---
+++ ifb/preparations/genome_indexing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=fast
+#SBATCH --job-name=Indexing
+#SBATCH --mem=16G
+#SBATCH --ntasks=1
+#SBATCH -t 1-0:00
+#SBATCH --cpus-per-task=8
+#SBATCH --output=indexing.out
+#SBATCH --error=indexing.err
+
+
+module load samtools/1.18
+
+#Path to the reference genome
+reference="/scratch/rd3725/nanopore/ref/GDDH13_1-1_formatted.fasta"
+
+samtools faidx -@ 8 $reference
